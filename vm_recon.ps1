ee6f90504bd084c67f4a26e4f8a586f43a92cf31
OleksandrK <oleksandr.kaninskyy@gmail.com>
2022-07-27 11:29:13 +0300

    Update vm_recon.ps1
    Here was added quotes for adapter name.

--- vm_recon.ps1
+++ vm_recon.ps1
@@ -6,7 +6,7 @@
 $vm_name='Minicube'
 
 # MY_IP
-$ip = "192.168.1.123"
+$vm_ip = "192.168.1.123"
 
 #########################################################
 #https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-controlvm.html
@@ -23,7 +23,7 @@ $nic2 = "Realtek USB GbE Family Controller"
 
 #pinging MY_IP by .Net
 $res = [System.Net.NetworkInformation.Ping]::new().Send($ip).Status
-Write-Host "Vm is UP:"$ip $res
+Write-Host "Vm is UP:"$vm_ip $res
 
 
 #verify if  $nic2 is UP
@@ -33,14 +33,14 @@ if ($res -ne "Success")
 {
     if ((Get-NetAdapter -InterfaceDescription $nic2 -Physical).Status -eq "Up")
     {
-       & echo "Briged adapter is " $nic2
+       & echo "Briged adapter is `"$nic2`""
        &"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $vm_name nic1 nat $nic2   
        Start-Sleep -m 500  
        &"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $vm_name nic1 bridged $nic2
     } 
     else
     {
-       & echo "Briged adapter is " $nic1
+       & echo "Briged adapter is `"$nic1`""
        &"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $vm_name nic1 nat $nic1   
        Start-Sleep -m 500  
        &"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $vm_name nic1 bridged $nic1
